01526a0ead2dbb19b808f5214b39e605af734321
ingbeto <ing.beto.rojas@gmail.com>
2020-10-12 01:43:24 -0500

    login y plantilla

---
+++ resources/views/home.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.admin')
+@section('content')
+    <div class="col-md-12">
+        <div class="alert alert-dismissible" style="background-color: #FFD700;" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span style="color:black" aria-hidden="true">×</span></button>
+             <strong style="color:black">Bienvenid@  al Sitio Oficial de LANIKORERA. Feliz Día, El Señor te Bendiga en Abundancia!</strong>
+        </div>
+    </div>
+    <div class="col-md-12">
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                <div class="info-box  hover-zoom-effect hover-expand-effect" style="background-color: #38383A; color:white">
+                    <div class="icon">
+                        <a href="{{route('usuario.vistacontrasenia')}}"><i class="material-icons">vpn_key</i></a>
+                    </div>
+                    <div class="content">
+                        <div style="color:white" class="text">CAMBIAR</div>
+                        <div style="color:white" class="number">CONTRASEÑA</div>
+                    </div>
+                </div>
+            </div>
+
+            @if(session()->exists('MOD_USUARIOS'))
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                    <div class="info-box hover-zoom-effect hover-expand-effect" style="background-color: #38383A; color:white">
+                        <div class="icon">
+                            <a href="{{route('admin.usuarios')}}"><i class="material-icons">person</i></a>
+                        </div>
+                        <div class="content">
+                            <div style="color:white" class="text">ADMINISTRACIÓN DE</div>
+                            <div style="color:white" class="number">USUARIOS</div>
+                        </div>
+                    </div>
+                </div>
+            @endif
+            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                <div class="info-box bg-red hover-zoom-effect hover-expand-effect">
+                    <div class="icon">
+                        <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i class="material-icons">exit_to_app</i></a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            {{ csrf_field() }}
+                        </form>
+                    </div>
+                    <div class="content">
+                        <div class="text">SALIR</div>
+                        <div class="number">DEL PANEL</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+//cambiar contraseña
+Route::get('usuarios/contrasenia/cambiar', 'UsuarioController@vistacontrasenia')->name('usuario.vistacontrasenia');
+Route::post('usuarios/contrasenia/cambiar/finalizar', 'UsuarioController@cambiarcontrasenia')->name('usuario.cambiarcontrasenia');
+Route::post('usuarios/contrasenia/cambiar/admin/finalizar', 'UsuarioController@cambiarPass')->name('usuario.cambiarPass');
+
+//TODOS LOS MENUS
+//GRUPO DE RUTAS PARA LA ADMINISTRACIÓN
+Route::group(['middleware' => 'auth', 'prefix' => 'admin'], function () {
+    date_default_timezone_set('America/Bogota');
+    Route::get('usuarios', 'MenuController@usuarios')->name('admin.usuarios');
+    Route::post('acceso', 'HomeController@confirmaRol')->name('rol');
+    Route::get('inicio', 'HomeController@inicio')->name('inicio');
+
+    //NOTIFICACIONES
+    Route::resource('notificaciones', 'NotificacionController');
+});
