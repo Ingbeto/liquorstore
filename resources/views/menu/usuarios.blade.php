d4658db8075ee2942a21a74e50d8a45560b3ddda
ingbeto <ing.beto.rojas@gmail.com>
2020-10-13 14:50:58 -0500

    detalles

--- resources/views/menu/usuarios.blade.php
+++ resources/views/menu/usuarios.blade.php
@@ -20,63 +20,11 @@
                     <strong style="color: black">Detalles: </strong > <span style="color: black">Agregue usuarios al sistema y administre sus privilegios, gestione los usuarios, configure los grupos de usuarios, así como también los módulos del sistema, entre otras tareas.</span>
                 </div>
                 <div class="button-demo">
-                    @if(session()->exists('PAG_MODULOS'))
-                    <a href="{{route('modulo.index')}}" class="btn  btn-lg  waves-effect" style="background-color: #38383A; color:white;">
-                        <div>
-                            <span><i class="material-icons">view_module</i></span>
-                            <span>MÓDULOS DEL SISTEMA</span>
-                            <span class="ink animate"></span></div>
-                    </a>
-                    @endif
-                    @if(session()->exists('PAG_PAGINAS'))
-                    <a href="{{route('pagina.index')}}" class="btn btn-lg  waves-effect" style="background-color: #38383A; color:white;">
-                        <div>
-                            <span><i class="material-icons">pages</i></span>
-                            <span>PÁGINAS DEL SISTEMA</span>
-                            <span class="ink animate"></span></div>
-                    </a>
-                    @endif
-                    @if(session()->exists('PAG_GRUPOS-ROLES'))
-                    <a href="{{route('grupousuario.index')}}" class="btn  btn-lg  waves-effect" style="background-color: #38383A; color:white;">
-                        <div>
-                            <span><i class="material-icons">group</i></span>
-                            <span>GRUPOS O ROLES DE USUARIOS</span>
-                            <span class="ink animate"></span>
-                        </div>
-                    </a>
-                    @endif
-                    @if(session()->exists('PAG_PRIVILEGIOS'))
-                    <a href="{{route('grupousuario.privilegios')}}" class="btn btn-lg waves-effect" style="background-color: #38383A; color:white;">
-                        <div>
-                            <span><i class="material-icons">desktop_access_disabled</i></span>
-                            <span>PRIVILÉGIOS A PÁGINAS</span>
-                            <span class="ink animate"></span>
-                        </div>
-                    </a>
-                    @endif
-                    @if(session()->exists('PAG_USUARIOS'))
-                    <a href="{{route('usuario.index')}}" class="btn btn-lg  waves-effect" style="background-color: #38383A; color:white;" data-toggle="tooltip" data-placement="top" title="Tenga en cuenta que al cargar gran cantidad de registros puede hacer que el navegador se bloquee y deba esperar a que este cargue todos los registros de la base de datos para continuar la navegación.">
-                        <div>
-                            <span><i class="material-icons">list</i></span>
-                            <span>LISTAR TODOS LOS USUARIOS</span>
-                            <span class="ink animate"></span>
-                        </div>
-                    </a>
-                    @endif
                     @if(session()->exists('PAG_USUARIO-MANUAL'))
                     <a href="{{route('usuario.create')}}" class="btn   btn-lg  waves-effect" style="background-color: #38383A; color:white;">
                         <div>
                             <span><i class="material-icons">book</i></span>
                             <span>USUARIO MANUAL</span>
-                            <span class="ink animate"></span>
-                        </div>
-                    </a>
-                    @endif
-                    @if(session()->exists('PAG_AUTOMATICO'))
-                    <a class="btn   btn-lg  waves-effect" style="background-color: #38383A; color:white;" disabled='disabled'>
-                        <div>
-                            <span><i class="material-icons">face</i></span>
-                            <span>USUARIO AUTOMATICO</span>
                             <span class="ink animate"></span>
                         </div>
                     </a>
