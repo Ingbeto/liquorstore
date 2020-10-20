eb64dbee820e076470c079b57a8be6805289fd74
Bmez1 <daniel.bmez1@gmail.com>
2020-10-19 19:56:47 -0500

    agregar y eliminar marcas

--- app/Http/Controllers/MarcasController.php
+++ app/Http/Controllers/MarcasController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Marcas;
+use App\Producto;
 use Illuminate\Http\Request;
 
 class MarcasController extends Controller
@@ -62,4 +63,76 @@ class MarcasController extends Controller
 
     }
 
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Marcas  $marcas
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Marcas $marcas)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Marcas  $marcas
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $marca = Marcas::findOrFail($id);
+        $location = 'almacen';
+        return view('almacen.marcas.edit',compact('marca','location'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Marcas  $marcas
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request,  $id)
+    {
+
+        $marca =  Marcas::findOrFail($id);
+        $marca->nombre = $request->nombre;
+        $result = $marca->save();
+        if($result){
+            flash("La Marca <strong>" . $marca->nombre . "</strong> fue actualizada de forma exitosa!")->success();
+            return  redirect()->route('marcas.index');
+        }else{
+            flash("La Marca <strong>" . $marca->nombre . "</strong> no fue actualizada de forma exitosa!")->error();
+            return  redirect()->back();
+        }
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Marcas  $marcas
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $marca = Marcas::findOrFail($id);
+        $exist = Producto::where('marca_id',$marca->id)->first();
+        if(!$exist){
+            $result = $marca->delete();
+
+            if($result){
+                flash("La Marca fue eliminada Correctamente")->success();
+                return  redirect()->back();
+            }else{
+                flash("La Marca no fue eliminada Correctamente")->error();
+                return  redirect()->back();
+            }
+        }else{
+            flash("No se puede eliminar la Marca ya que tiene productos asociados")->error();
+            return  redirect()->back();
+        }
+    }
 }
