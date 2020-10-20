6eaa4c19ff58728020221296bf738b1a86ad92eb
CarlosTuiran <ctuiran@unicesar.edu.co>
2020-10-19 20:13:08 -0500

    categoria y subcategoria editar y eliminar

--- app/Http/Controllers/CategoriasController.php
+++ app/Http/Controllers/CategoriasController.php
@@ -8,22 +8,14 @@ use Illuminate\Http\Request;
 
 class CategoriasController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         $categorias  = Categorias::all();
         return view('almacen.categorias.list')->with('categoria',$categorias)->with('location','almacen');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function create()
     {
         $location = 'almacen';
@@ -49,4 +41,55 @@ class CategoriasController extends Controller
             return  redirect()->back();
         }
     }
+
+
+    public function show(Categorias $categoria)
+    {
+        //
+    }
+
+
+    public function edit($id)
+    {
+        $categoria = Categorias::findOrFail($id);
+        $location = 'almacen';
+        return view('almacen.categorias.edit',compact('categoria','location'));
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        $categoria =  Categorias::findOrFail($id);
+        $categoria->nombre = $request->nombre;
+        $result = $categoria->save();
+        if($result){
+            flash("La Categoría <strong>" . $categoria->nombre . "</strong> fue actualizada de forma exitosa!")->success();
+            return  redirect()->route('categorias.index');
+        }else{
+            flash("La Categoría <strong>" . $categoria->nombre . "</strong> no fue actualizada de forma exitosa!")->error();
+            return  redirect()->back();
+        }
+    }
+
+
+    public function destroy($id)
+    {
+        $categoria = Categorias::findOrFail($id);
+        $exist = Producto::where('categoria_id',$categoria->id)->first();
+        if(!$exist){
+            $result = $categoria->delete();
+
+            if($result){
+                flash("La Categoria fue eliminada Correctamente")->success();
+                return  redirect()->back();
+            }else{
+                flash("La Categoria no fue eliminada Correctamente")->error();
+                return  redirect()->back();
+            }
+        }else{
+            flash("No se puede eliminar la Categoria ya que tine productos asociados")->error();
+            return  redirect()->back();
+        }
+    }
+
 }
